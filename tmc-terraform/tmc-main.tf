cee39d86afda472d7b7c20df07d19752e6b3aaf8
Rohan Mukesh <rohanmukesh12@gmail.com>
2023-08-23 13:42:49 -0500

    Update tmc-main.tf

--- tmc-terraform/tmc-main.tf
+++ tmc-terraform/tmc-main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     tanzu-mission-control = {
       source = "vmware/tanzu-mission-control"
-      version = "1.0.1"    # it's the provider version and you can change it as version changes
+      version = "1.2.1"    # it's the provider version and you can change it as version changes
     }
   }
 }
